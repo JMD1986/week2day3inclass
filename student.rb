6858f854bdb8f0c1996398d4fb2b8ed6ae30d385
John Daugherty <johndaugherty1986@gmail.com>
2015-05-13 13:17:18 -0400

    in class assignment

--- student.rb
+++ student.rb
@@ -1,12 +1,25 @@
 class Student
 
-  attr_accessor :name, :course,
-  attr_reader :GPA
+  attr_accessor :name, :course
+  attr_reader :gpa
 
   def intialize(name, course, gpa)
     @name = name
     @course = course
     @gpa = gpa
+    students = [
+        Student.new('Stan', 'Rails', 3.3),
+        Student.new('Jan', 'Rails', 3.5),
+        Student.new('Fran', 'Java', 2.9),
+        Student.new('Denise', 'HTML', 3.1),
+        Student.new('Pan', 'Javascript', 3.5),
+        Student.new('Shaniqua', 'Ruby', 3.1),
+        Student.new('Chad', 'Rails', 3.2),
+        Student.new('Jennifer', 'HTML', 3.0),
+        Student.new('Robert', 'Javascript', 2.8),
+        Student.new('Sarah', 'drinking', 3.1)
+      ]
+
   end
 
   def turn_in_homework
@@ -20,18 +33,4 @@ class Student
   def learn
     puts "listen to lecture. code during lab. +10 knowledge"
   end
-
-student_body = [
-    Student.new('Stan', 'Rails', 3.3)
-    Student.new('Jan', 'C++', 3.5)
-    Student.new('Fran', 'Java', 2.9)
-    Student.new('Denise', 'HTML', 3.1)
-    Student.new('Pan', 'Javascript', 3.5)
-    Student.new('Shaniqua', 'Ruby', 3.1)
-    Student.new('Chad', 'Rails', 3.2)
-    Student.new('Jennifer', 'HTML', 3.0)
-    Student.new('Robert', 'Javascript', 2.8)
-    Student.new('Sarah', 'drinking', 3.1)
-  ]
-
 end
